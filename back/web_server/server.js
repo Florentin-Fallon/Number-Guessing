756d743a01633b9f370d33a26f68032ca478140d
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2023-03-17 10:10:53 +0100

    create filder back & front

--- back/web_server/server.js
+++ back/web_server/server.js
@@ -5,7 +5,7 @@ const host = 'localhost';
 const server = http.createServer(function (req, res) {
     const url = req.url;
     if (url === '/'){
-        res.end("Bonjour vous êtes bien sur le serveur de florentin ")
+        res.end("Bonjour vous êtes bien sur le serveur de number-guessing")
     }
 });
 
